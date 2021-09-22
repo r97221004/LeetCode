e604cc66971af4119fd770dc9958840bc4cf7f1c
r97221004 <r97221004@ntu.edu.tw>
2021-09-22 22:48:35 +0800

    Add 035_Search_Insert_Position

---
+++ 035_Search_Insert_Position.py
@@ -0,0 +1,29 @@
+# 時間複雜度: O(n)
+# class Solution:
+#     def searchInsert(self, nums, target):
+#         for i in range(len(nums)):
+#             if nums[i] >= target:
+#                 return i
+            
+#         return len(nums)
+
+# p = Solution()
+# print(p.searchInsert([1,3,5,6], 5))  
+
+
+# Classical binary search  
+# 時間複雜度 O(log(n)):
+class Solution:
+    def searchInsert(self, nums, target):
+        beg, end = 0, len(nums) # beg 會是我們最終要插入的位置  
+        while beg < end:
+            mid = (beg + end)//2
+            if nums[mid] >= target:
+                end = mid
+            else:
+                beg = mid + 1
+        return beg
+
+
+p = Solution()
+print(p.searchInsert([1,3,5,6], 5))  
