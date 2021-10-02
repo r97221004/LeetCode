ec8931e8a1814c5e34702d44940b8f7a05d180da
r97221004 <r97221004@ntu.edu.tw>
2021-10-02 19:15:50 +0800

    Add 136_Single_Number

---
+++ 136_Single_Number.py
@@ -0,0 +1,29 @@
+# Bit Manipulation
+# 時間複雜度: O(n)
+# 空間複雜度: O(1)
+# 概念:
+# a^0 = a 且 a^a=0 且 a^b^a = a^a^b = 0^b = b 
+
+class Solution:
+    def singleNumber(self, nums):
+        a = 0
+        for i in nums:
+            a ^= i
+        return a
+p = Solution()
+print(p.singleNumber( [4,1,2,1,2]))    
+
+
+# 時間複雜度: O(n)
+# 空間複雜度: O(n)
+class Solution:
+    def singleNumber(self, nums):
+        data = {}
+        for i in nums:
+            data[i] = data.get(i, 0) + 1
+        for i in data.keys():
+            if data[i] == 1:
+                return i
+
+p = Solution()
+print(p.singleNumber( [4,1,2,1,2]))   
