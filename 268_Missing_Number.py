cc19cf9c63bd272f850d22df22978149818b5346
r97221004 <r97221004@ntu.edu.tw>
2021-10-02 20:55:57 +0800

    Add 268_Missing_Number

---
+++ 268_Missing_Number.py
@@ -0,0 +1,28 @@
+# Bit Manipulation
+# 時間複雜度: O(n)
+# 空間複雜度: O(1)
+class Solution:
+    def missingNumber(self, nums):
+        a = len(nums)
+        for i, val in enumerate(nums):
+            a ^= i^val
+        
+        return a
+
+p = Solution()
+print(p.missingNumber([9,6,4,2,3,5,7,0,1]))
+
+# 時間複雜度: O(n)
+# 空間複雜度: O(n)
+class Solution:
+    def missingNumber(self, nums):
+        data = {}
+        for i in nums:
+            data[i] = 1
+        
+        for i in range(0, len(nums) + 1):
+            if i not in data:
+                return i
+
+p = Solution()
+print(p.missingNumber([9,6,4,2,3,5,7,0,1]))
