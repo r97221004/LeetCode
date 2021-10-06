8eca562bc93343f4fe278c973d5fb75ff3d2ccee
r97221004 <r97221004@ntu.edu.tw>
2021-10-06 20:56:03 +0800

    350_Intersection_of_Two

---
+++ 350_Intersection_of_Two.py
@@ -0,0 +1,23 @@
+# 時間複雜度: O(n + m), n = len(nums1) 且 m = len(nums2)
+# 空間複雜度:O(min(n, m))
+
+class Solution:
+    def intersect(self, nums1, nums2):
+        if len(nums2) < len(nums1):
+            nums1, nums2 = nums2, nums1
+
+        count = {}
+        result = []
+        
+        for i in nums1:
+            count[i] = count.get(i, 0) + 1
+            
+        for i in nums2:
+            if i in count and count[i] > 0:
+                result.append(i)
+                count[i] -= 1
+                
+        return result
+
+p = Solution()
+print(p.intersect([1,2,2,1], [2,2]))
