e98f40cb53c86026ea7af7d865475b7faa1edb9d
r97221004 <r97221004@ntu.edu.tw>
2021-10-06 20:20:24 +0800

    349_Intersection_of_Two_Arrays

---
+++ 349_Intersection_of_Two_Arrays.py
@@ -0,0 +1,23 @@
+# 時間複雜度: O(n + m), n = len(nums1) 且 m = len(nums2)
+# 空間複雜度: O(n + m)
+# 注意像 set1 = set(nums1) 這種把列表轉集合的操作時間複雜度是: O(n)
+class Solution:
+    def intersection(self, nums1, nums2):
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return list(set1 & set2)
+p = Solution()
+print(p.intersection([1,2,2,1], [2,2]))
+
+
+# 時間複雜度: O(n*m), n = len(nums1) 且 m = len(nums2)
+# 空間複雜度: O(min(n, m))
+class Solution:
+    def intersection(self, nums1, nums2):
+        result = []
+        for i, val in enumerate(nums1):
+            if val in nums2 and val not in result:
+                result.append(val)
+        return result
+p = Solution()
+print(p.intersection([1,2,2,1], [2,2]))
