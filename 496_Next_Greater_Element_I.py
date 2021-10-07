e946d424513e84232b16340e54c666ed48dd5701
r97221004 <r97221004@ntu.edu.tw>
2021-10-07 20:18:14 +0800

    496_Next_Greater_Element_I

---
+++ 496_Next_Greater_Element_I.py
@@ -0,0 +1,46 @@
+# Brute Force 暴力法
+# 時間複查度 O(n*m), m = len(nums1) 且 n = len(nums2)
+# 空間複雜度: O(n), 注意 result 是我們必要的返回結果，所以我們這裡可以不用算進去空間複雜度
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        result = []
+        data = {}
+        for i, val in enumerate(nums2):
+            data[val] = i
+            
+        for i, val in enumerate(nums1):
+            for j in range(data[val] + 1, len(nums2)):
+                if nums2[j] > val:
+                    result.append(nums2[j])
+                    break
+            
+            else:
+                result.append(-1)
+        
+        return result
+
+p = Solution()
+print(p.nextGreaterElement([4,1,2], [1,3,4,2]))
+
+# stack
+# 時間複雜度: O(n), n = len(nums2) 且注意 nums1 是 nums2 的子集合，所以數量一定小於等於 n
+# 空間複雜度: O(n)
+class Solution:
+    def nextGreaterElement(self, nums1, nums2):
+        data = {}
+        stack = []
+        answer = []
+        
+        for i in nums2:
+            while stack and stack[-1] < i:
+                data[stack.pop()] = i
+            stack.append(i)
+            
+        for i in nums1:
+            answer.append(data.get(i, -1))
+        
+        return answer
+
+p = Solution()
+print(p.nextGreaterElement([4,1,2], [1,3,4,2]))
+
