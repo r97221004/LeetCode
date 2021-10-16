43c5ade9e880645866fe1f57dab86293d04e3c2c
r97221004 <r97221004@ntu.edu.tw>
2021-10-16 20:48:38 +0800

    Add 1431_Kids_With_the_Greatest_Number_of_Candies

---
+++ 1431_Kids_With_the_Greatest_Number_of_Candies.py
@@ -0,0 +1,17 @@
+# 時間複雜度: O(n)
+# 空間複雜度: O(1)
+class Solution:
+    def kidsWithCandies(self, candies, extraCandies):
+        mx = max(candies)
+        
+        for i, val in enumerate(candies):
+            if val + extraCandies >= mx:
+                candies[i] = True
+            
+            else:
+                candies[i] = False
+                
+        return candies
+
+p = Solution()
+print(p.kidsWithCandies([2,3,5,1,3], 3))
