729d3e9a46f0fd6aef58679f44e13564bbfddb24
r97221004 <r97221004@ntu.edu.tw>
2021-10-17 16:13:10 +0800

    Add 1480_Running_Sum_of_1d_Array

---
+++ Array/1480_Running_Sum_of_1d_Array.py
@@ -0,0 +1,14 @@
+# 時間複雜度: O(n)
+# 空間複雜度: O(1)
+class Solution:
+    def runningSum(self, nums):
+        prefix_sum = 0
+        
+        for i, val in enumerate(nums):
+            prefix_sum += val
+            nums[i] = prefix_sum
+        
+        return nums
+
+p = Solution()
+print(p.runningSum([1,2,3,4]))
