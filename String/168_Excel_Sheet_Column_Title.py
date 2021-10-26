c137140c0737a166130f1333e69a1952fac77f7d
r97221004 <r97221004@ntu.edu.tw>
2021-10-26 18:53:47 +0800

    Add String/168_Excel_Sheet_Column_Title

---
+++ String/168_Excel_Sheet_Column_Title.py
@@ -0,0 +1,15 @@
+# 時間複雜度: O(log(n)), n = columnNumber
+# 空間複雜度: O(1), 不算 resultclass Solution:
+class Solution:
+    def convertToTitle(self, columnNumber):
+        capitals = [chr(i) for i in range(ord('A'), ord('Z') + 1)]  # ord gets the ascii value from a character and chr vice versa.
+        result = ''
+        
+        while columnNumber:
+            columnNumber, result = (columnNumber-1)//26, capitals[(columnNumber-1)%26] + result  
+            
+        return result
+    
+p = Solution()
+print(p.convertToTitle(28))
+
