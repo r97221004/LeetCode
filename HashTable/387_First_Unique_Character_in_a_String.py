6a51319be20457a761207533e694939aa8040398
r97221004 <r97221004@ntu.edu.tw>
2021-10-28 17:50:25 +0800

    Add 387_First_Unique_Character_in_a_String

---
+++ HashTable/387_First_Unique_Character_in_a_String.py
@@ -0,0 +1,17 @@
+# 時間複雜度: O(n)
+# 空間複雜度: O(1), 英文字母最多 26 個
+class Solution:
+    def firstUniqChar(self, s):
+        mapping = {}
+        
+        for i in s:
+            mapping[i] = mapping.get(i, 0) + 1
+            
+        for i, val in enumerate(s):
+            if mapping[val] == 1:
+                return i
+            
+        return -1
+
+p = Solution()
+print(p.firstUniqChar("leetcode"))
