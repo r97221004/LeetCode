87288eca44ee3fad64bc959b89541edb9066a4b0
r97221004 <r97221004@ntu.edu.tw>
2021-11-06 17:08:22 +0800

    Add 448_Find_All_Numbers_Disappeared_in_an_Array

---
+++ Array/448_Find_All_Numbers_Disappeared_in_an_Array.py
@@ -0,0 +1,12 @@
+# 時間複雜度: O(n)
+# 空間複雜度: O(n)
+# 注意，set() 的時間複雜度是 O(n)，空間複雜度也是 O(n)
+class Solution:
+    def findDisappearedNumbers(self, nums):
+        lgh = len(nums)
+        nums = set(nums)
+        
+        return [ i for i in range(1, lgh + 1) if i not in nums ]
+
+p = Solution()
+print(p.findDisappearedNumbers([4,3,2,7,8,2,3,1]))
