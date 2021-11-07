3a9c21c273d236808c017dcaa9a4d3f0ae5d4bf7
r97221004 <r97221004@ntu.edu.tw>
2021-11-07 16:36:56 +0800

    Add 338_Counting_Bits

---
+++ DynamicProgramming/338_Counting_Bits.py
@@ -0,0 +1,17 @@
+# 時間複雜度: O(n)
+# 空間複雜度: O(1)
+# All whole numbers can be represented by 2N (even) and 2N + 1 (odd).
+# For a given binary number, multiplying by 2 is the same as adding a zero at the end (just as we just add a zero when multiplying by ten in base 10).
+# countBits(N) = countBits(2N)
+# countBits(N) + 1 = countBits(2N + 1)
+class Solution:
+    def countBits(self, n):
+        ans = [0]*(n + 1)
+        
+        for i in range(1, n + 1):
+            ans[i] = ans[i//2] + i%2
+            
+        return ans
+
+p = Solution()
+print(p.countBits(5))
