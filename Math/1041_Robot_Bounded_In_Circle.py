bdf6fc6df56f6cb3f06541af4eaa4ecd6455421a
r97221004 <r97221004@ntu.edu.tw>
2021-11-26 18:40:56 +0800

    Add 1041_Robot_Bounded_In_Circle

---
+++ Math/1041_Robot_Bounded_In_Circle.py
@@ -0,0 +1,22 @@
+# 時間複雜度: O(n)
+# 空間複雜度: O(1)
+class Solution:
+    def isRobotBounded(self, instructions):
+        x = y = 0
+        direction = 0
+        possible_moves = {0: [0, 1], 1: [1, 0], 2: [0, -1], 3: [-1, 0]}
+
+        for instruction in instructions:
+            if instruction == "L":
+                direction = (direction + 3)%4
+            elif instruction == "R":
+                direction = (direction + 1)%4
+        
+            else:
+                x = x + possible_moves[direction][0]
+                y = y + possible_moves[direction][1]
+        
+        return (x == 0 and y ==0) or direction != 0
+
+p = Solution()
+print(p.isRobotBounded("GGLLGG"))
