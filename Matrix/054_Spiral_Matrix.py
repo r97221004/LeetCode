65a11b999be9bdd509e2bfcb94c90f5255d78ecc
r97221004 <r97221004@ntu.edu.tw>
2021-11-30 18:20:53 +0800

    Add 054_Spiral_Matrix

---
+++ Matrix/054_Spiral_Matrix.py
@@ -0,0 +1,12 @@
+# 時間複雜度: ?
+# 空間複雜度: O(1)
+class Solution:
+    def spiralOrder(self, matrix):
+        res = []
+        while matrix:
+            res.extend(matrix.pop(0))
+            matrix = list(zip(*matrix))[::-1]
+        return res
+p = Solution()
+print(p.spiralOrder( [[1,2,3],[4,5,6],[7,8,9]]))
+            
