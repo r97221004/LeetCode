69a0cf750715c7ec547dab37329423061b51abbd
r97221004 <r97221004@ntu.edu.tw>
2021-12-02 21:30:55 +0800

    Add 809_Expressive_Words

---
+++ TwoPointers/809_Expressive_Words.py
@@ -0,0 +1,17 @@
+# 時間複雜度:O(n)
+# 空間複雜度: O(n) ?
+class Solution:
+    def expressiveWords(self, s: str, words):
+        return sum(self.check(s, w) for w in words)
+            
+    def check(self, s, w):
+        i, j, n, m = 0, 0, len(s), len(w)
+        for i in range(n):
+            if j < m and s[i] == w[j]: j += 1
+            elif s[i - 1:i + 2] != s[i]*3 != s[i - 2:i + 1]: return False
+        
+        return j == m
+
+p = Solution()
+print(p.expressiveWords("heeellooo", ["hello", "hi", "helo"]))
+        
