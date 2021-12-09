e3f20372e7600df2afd2a54147abdf41b0875b83
r97221004 <r97221004@ntu.edu.tw>
2021-12-09 20:44:01 +0800

    Add 570_Managers_with_at_Least_5_Direct_Reports

---
+++ Database/570_Managers_with_at_Least_5_Direct_Reports.sql
@@ -0,0 +1,9 @@
+select name
+from Employee 
+where id in 
+
+        (
+            select managerId
+            from Employee
+            group by managerId
+            having count(managerId) >= 5 );
