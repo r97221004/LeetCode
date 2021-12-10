23b8f10a8944fe3543c180631c0d52e2d6608270
r97221004 <r97221004@ntu.edu.tw>
2021-12-10 21:13:09 +0800

    Add 602_Friend_Requests_II_Who_Has_the_Most_Friends

---
+++ Database/602_Friend_Requests_II_Who_Has_the_Most_Friends.sql
@@ -0,0 +1,15 @@
+with cte as (
+   
+    select requester_id as id from RequestAccepted
+    
+    union all
+
+    select accepter_id as id from RequestAccepted
+)
+
+
+select id, count(*) as num
+from cte
+group by id 
+order by count(*) desc
+limit 1;
