c89e589ccf18b2ead9da206a506b47e6d697bf64
r97221004 <r97221004@ntu.edu.tw>
2021-12-18 22:06:12 +0800

    Add 1308_Running_Total_for_Different_Genders

---
+++ Database/1308_Running_Total_for_Different_Genders.sql
@@ -0,0 +1,6 @@
+select s1.gender,
+       s1.day,
+       sum(s2.score_points) as total
+from Scores as s1 join Scores as s2 on s1.gender = s2.gender and s1.day >= s2.day
+group by s1.gender, s1.day 
+order by s1.gender, s1.day;
