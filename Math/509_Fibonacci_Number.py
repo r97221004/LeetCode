a71ce612c8a082affa377d216467304ae54d079d
r97221004 <r97221004@ntu.edu.tw>
2021-12-25 21:12:53 +0800

    更新程式碼

--- Math/509_Fibonacci_Number.py
+++ Math/509_Fibonacci_Number.py
@@ -12,3 +12,16 @@ class Solution:
         
 p = Solution()
 print(p.fib(4))
+
+
+# 時間複雜度: O(2**n)
+# 空間複雜度: O(n)
+class Solution:
+    def fib(self, n):
+        if n <= 1:
+            return n
+        
+        return self.fib(n - 1) + self.fib(n - 2)
+        
+p = Solution()
+print(p.fib(4))
