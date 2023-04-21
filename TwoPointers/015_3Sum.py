4a7ae787c6adae5b1aba775f9b14671c359376ce
r97221004 <r97221004@ntu.edu.tw>
2023-04-21 10:26:17 +0800

    update 015

--- TwoPointers/015_3Sum.py
+++ TwoPointers/015_3Sum.py
@@ -29,3 +29,32 @@ class Solution:
                     
 p = Solution()
 print(p.threeSum([-1, 0, 1, 2, -1, -4]))
+
+
+class Solution:
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+
+        for index, val in enumerate(nums):
+            if index > 0 and nums[index - 1] == nums[index]:
+                continue
+
+            left = index + 1
+            right = len(nums) - 1
+            while left < right:
+                threeSum = val + nums[left] + nums[right]
+                if threeSum > 0:
+                    right -= 1
+                elif threeSum < 0:
+                    left += 1
+                else:
+                    res.append([val, nums[left], nums[right]])
+                    left += 1
+                    while left < right and nums[left - 1] == nums[left]:    
+                        left += 1
+        return res
+
+                    
+p = Solution()
+print(p.threeSum([-1, 0, 1, 2, -1, -4]))
