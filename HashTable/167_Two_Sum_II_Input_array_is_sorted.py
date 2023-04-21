f5835b357ecbe7165a37f4a7ff156dbc73833131
r97221004 <r97221004@ntu.edu.tw>
2023-04-21 09:30:34 +0800

    update 167

--- HashTable/167_Two_Sum_II_Input_array_is_sorted.py
+++ HashTable/167_Two_Sum_II_Input_array_is_sorted.py
@@ -31,3 +31,22 @@ class Solution:
 
 p = Solution()
 print(p.twoSum([2, 7, 11, 15], 9))
+
+
+class Solution:
+    def twoSum(self, numbers, target):
+        left = 0
+        right = len(numbers) - 1
+
+        while left < right:
+            curSum = numbers[left] + numbers[right]
+            if target - curSum > 0: left += 1
+            elif target - curSum < 0: right -= 1
+            else: return [left + 1, right + 1]
+
+        return []    
+
+
+
+p = Solution()
+print(p.twoSum([2, 7, 11, 15], 9))
