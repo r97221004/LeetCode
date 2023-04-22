4396bb13e589cc7a7b26c8a50ab8ac8eb0ff0dcf
chiahao <r97221004@gmail.com>
2023-04-22 20:28:17 +0800

    update 704

--- BinarySearch/704_Binary_Search.py
+++ BinarySearch/704_Binary_Search.py
@@ -8,27 +8,29 @@ class Solution:
 
         return -1
 
+
 p = Solution()
 print(p.search([-1, 0, 3, 5, 9, 12], 9))
 
 
-# Binary search  
+# Binary search
 # 時間複雜度: O(log(n))
 # 空間複雜度: O(1)
 class Solution:
     def search(self, nums, target):
-        start = 0
-        end = len(nums)
-        
-        while start < end:
-            mid = (start + end)//2
-            
-            if nums[mid] >= target:
-                end = mid              
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (right + left)//2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
             else:
-                start = mid + 1
-                
-        return start if start < len(nums) and nums[start] == target else -1
+                left = mid + 1
+        return -1
+
 
 p = Solution()
 print(p.search([-1, 0, 3, 5, 9, 12], 9))
