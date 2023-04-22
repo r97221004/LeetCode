e8f3e5a389bef24f5c43478bc21ca16ec52a4510
chiahao <r97221004@gmail.com>
2023-04-22 18:49:47 +0800

    update 338

--- BitManipulation/338_Counting_Bits.py
+++ BitManipulation/338_Counting_Bits.py
@@ -7,11 +7,33 @@
 class Solution:
     def countBits(self, n):
         ans = [0]*(n + 1)
-        
+
         for i in range(1, n + 1):
-            ans[i] = ans[i//2] + i%2
-            
+            ans[i] = ans[i//2] + i % 2
+
         return ans
+
+
+p = Solution()
+print(p.countBits(5))
+
+
+# 時間複雜度: O(n•log(n))
+# 空間複雜度: O(1)
+class Solution:
+    def countBits(self, n):
+        result = []
+        tmp = 0
+
+        for i in range(n + 1):
+            while i:
+                tmp += i % 2
+                i = i >> 1
+            result.append(tmp)
+            tmp = 0
+
+        return result
+
 
 p = Solution()
 print(p.countBits(5))
