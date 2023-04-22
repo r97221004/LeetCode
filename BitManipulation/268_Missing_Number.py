66a41df24b8df96c3685b1856d1762b0cc2d5fbd
chiahao <r97221004@gmail.com>
2023-04-22 19:26:02 +0800

    update 268

--- BitManipulation/268_Missing_Number.py
+++ BitManipulation/268_Missing_Number.py
@@ -5,9 +5,10 @@ class Solution:
     def missingNumber(self, nums):
         res = len(nums)
         for i, val in enumerate(nums):
-            res ^= i^val
-        
+            res ^= i ^ val
+
         return res
+
 
 p = Solution()
 print(p.missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
@@ -20,10 +21,27 @@ class Solution:
         data = {}
         for i in nums:
             data[i] = 1
-        
+
         for i in range(0, len(nums) + 1):
             if i not in data:
                 return i
+
+
+p = Solution()
+print(p.missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
+
+
+# 時間複雜度: O(n)
+# 空間複雜度: O(1)
+class Solution:
+    def missingNumber(self, nums):
+        result = len(nums)
+
+        for index, val in enumerate(nums):
+            result += (index - val)
+
+        return result
+
 
 p = Solution()
 print(p.missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]))
