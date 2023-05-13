a2d2fc2f1d3fd5613eda780ed30bb8020a2c2839
chiahao <r97221004@gmail.com>
2023-05-13 16:16:07 +0800

    update 190

--- BitManipulation/190_Reverse_Bits.py
+++ BitManipulation/190_Reverse_Bits.py
@@ -9,3 +9,16 @@ class Solution:
             result = result | (tmp << 31 - i)
             n = n >> 1
         return result
+
+
+# 時間複雜度: O(n)
+# 空間複雜度: O(1)
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res = 0
+
+        for i in range(31, -1, -1):
+            res += n % 2*(2**i)
+            n = n >> 1
+
+        return res
