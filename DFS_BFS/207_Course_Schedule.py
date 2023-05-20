b58f0bfb3416a3762487a66410c085f0ca1c88d2
chiahao <r97221004@gmail.com>
2023-05-20 17:48:41 +0800

    add 207

---
+++ DFS_BFS/207_Course_Schedule.py
@@ -0,0 +1,34 @@
+# 時間複雜度: O(n + p)
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites):
+        preMap = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        visitSet = set()
+
+        def dfs(crs):
+            if crs in visitSet:
+                return False
+
+            if preMap[crs] == []:
+                return True
+
+            visitSet.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+
+            visitSet.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for crs in range(numCourses):
+            if not dfs(crs):
+                return False
+
+        return True
+
+
+p = Solution()
+print(p.canFinish(2, [[0, 1], [1, 0]]))
