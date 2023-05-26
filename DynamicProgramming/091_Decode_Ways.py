196089f079d1a3448c44bfd0304111d4e0e33b13
r97221004 <r97221004@ntu.edu.tw>
2023-05-26 11:23:39 +0800

    update 091

--- DynamicProgramming/091_Decode_Ways.py
+++ DynamicProgramming/091_Decode_Ways.py
@@ -11,7 +11,7 @@ class Solution:
             else:
                 dp[i] = dp[i + 1]
 
-            if (i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456")):
+            if (i + 2 <= len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456")):
                 dp[i] += dp[i + 2]
 
         return dp[0]
