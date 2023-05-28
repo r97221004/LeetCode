601cf768e142b6fe516ea68c5885b53a2aa720d2
chiahao <r97221004@gmail.com>
2023-05-28 22:40:26 +0800

    update 098

--- BST/098_Validate_Binary_Search_Tree.py
+++ BST/098_Validate_Binary_Search_Tree.py
@@ -4,6 +4,9 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# 時間複雜度: O(n)
+# 空間複雜度: O(n) + O(h)
 class Solution:
     def isValidBST(self, root):
 
@@ -27,3 +30,32 @@ class Solution:
                 traversal(current_node.right)
         traversal(root)
         return result
+
+
+# 時間複雜度: O(n)
+# 空間複雜度: O(h)
+class Solution:
+    def isValidBST(self, root):
+        return self.dfs(root)
+
+    def dfs(self, root):
+        # in order DFS
+        self.flag = 1
+        self.pre_node = None
+
+        def traversal(current_node):
+            if self.flag != 1:
+                return
+            if current_node.left is not None:
+                traversal(current_node.left)
+            # results.append(current_node.val)
+            if self.pre_node:
+                if self.pre_node.val >= current_node.val:
+                    self.flag = 0
+            self.pre_node = current_node
+
+            if current_node.right is not None:
+                traversal(current_node.right)
+
+        traversal(root)
+        return True if self.flag == 1 else False
