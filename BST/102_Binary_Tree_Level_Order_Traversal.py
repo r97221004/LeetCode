4778548e8d4b98ffd840f88d7e8b804eb2cb77ae
chiahao <r97221004@gmail.com>
2023-06-03 17:36:10 +0800

    add 102

---
+++ BST/102_Binary_Tree_Level_Order_Traversal.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+# 時間複雜度: O(n)
+# 空間複雜度: O(n/2)
+import collections
+
+
+class Solution:
+    def levelOrder(self, root):
+        res = []
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        return res
