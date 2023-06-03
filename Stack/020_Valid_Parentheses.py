732d6f24e687d35df404b0f1206d0481e4e4bb5c
chiahao <r97221004@gmail.com>
2023-06-03 20:12:30 +0800

    update 20

--- Stack/020_Valid_Parentheses.py
+++ Stack/020_Valid_Parentheses.py
@@ -2,37 +2,46 @@
 # 空間複雜度: O(n)
 class Solution:
     def isValid(self, s):
-        mapping = {'}':'{', ']':'[', ')':'('}
+        mapping = {'}': '{', ']': '[', ')': '('}
         stack = []
-        
+
         for i in s:
             if i in mapping.values():
                 stack.append(i)
-            
+
             elif i in mapping.keys():
-                if stack == [ ] or mapping[i] != stack.pop():
+                if stack == [] or mapping[i] != stack.pop():
                     return False
-                
+
         return stack == []
+
 
 p = Solution()
 print(p.isValid("()[]{}"))
 
 # 時間複雜度: O(n)
 # 空間複雜度: O(n)
+
+
 class Solution:
     def isValid(self, s):
         stack = []
-        pair = {")": "(", "}": "{", "]": "["}
-        
+        closeToOpen = {
+            ")": "(",
+            "]": "[",
+            "}": "{"
+        }
+
         for i in s:
-            if not stack or i not in pair.keys(): stack.append(i)
-            
+            if i in closeToOpen:
+                if stack and stack[-1] == closeToOpen[i]:
+                    stack.pop()
+                else:
+                    return False
             else:
-                if pair[i] == stack[-1]: stack.pop()
-                else: stack.append(i)
-                
-        return stack == []
+                stack.append(i)
+        return False if stack else True
+
 
 p = Solution()
 print(p.isValid("()[]{}"))
