9a4ca1560f4e4242a8808c18a39342097c6a5927
chiahao <r97221004@gmail.com>
2023-06-11 17:56:17 +0800

    add 355

---
+++ Heap/355_ Design_Twitter.py
@@ -0,0 +1,49 @@
+from collections import defaultdict
+import heapq
+
+
+class Twitter:
+    def __init__(self):
+        self.count = 0
+        # userId -> list of [count, tweetIds]
+        self.tweetMap = defaultdict(list)
+        # userId -> set of followeeId
+        self.followMap = defaultdict(set)
+
+    def postTweet(self, userId, tweetId) -> None:
+        self.tweetMap[userId].append([self.count, tweetId])
+        self.count -= 1
+
+    def getNewsFeed(self, userId):
+        res = []
+        minHeap = []
+        self.followMap[userId].add(userId)
+        for followeeId in self.followMap[userId]:
+            if followeeId in self.tweetMap:
+                index = len(self.tweetMap[followeeId]) - 1
+                count, tweetId = self.tweetMap[followeeId][index]
+                heapq.heappush(
+                    minHeap, [count, tweetId, followeeId, index - 1])
+        while minHeap and len(res) < 10:
+            count, tweetId, followeeId, index = heapq.heappop(minHeap)
+            res.append(tweetId)
+            if index >= 0:
+                count, tweetId = self.tweetMap[followeeId][index]
+                heapq.heappush(
+                    minHeap, [count, tweetId, followeeId, index - 1])
+        return res
+
+    def follow(self, followerId, followeeId) -> None:
+        self.followMap[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.followMap[followerId]:
+            self.followMap[followerId].remove(followeeId)
+
+
+# Your Twitter object will be instantiated and called as such:
+# obj = Twitter()
+# obj.postTweet(userId,tweetId)
+# param_2 = obj.getNewsFeed(userId)
+# obj.follow(followerId,followeeId)
+# obj.unfollow(followerId,followeeId)
