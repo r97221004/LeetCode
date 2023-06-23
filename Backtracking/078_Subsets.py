5f4598f785f144a8139a24131b0fc270444b0f7f
chiahao <r97221004@gmail.com>
2023-06-23 17:34:33 +0800

    update 078

--- Backtracking/078_Subsets.py
+++ Backtracking/078_Subsets.py
@@ -19,3 +19,25 @@ class Solution:
 
 p = Solution()
 print(p.subsets(nums=[1, 2, 3]))
+
+
+class Solution:
+    def __init__(self):
+        self.ans = []
+
+    def find(self, start, nums, subset):
+        if start >= len(nums):
+            self.ans.append(subset[:])
+            return
+        subset.append(nums[start])
+        self.find(start + 1, nums, subset)
+        subset.pop()
+        self.find(start + 1, nums, subset)
+
+    def subsets(self, nums):
+        self.find(0, nums, [])
+        return self.ans
+
+
+p = Solution()
+print(p.subsets(nums=[1, 2, 3]))
